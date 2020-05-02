37d76b572f8f45c04f5a86ce00fe237965137f65
vascubrian <twijukyevascubrian@gmail.com>
2020-05-02 20:01:06 +0300

    fix(feat) :fixing invalid email

--- public/src/UserAccountcontroller.js
+++ public/src/UserAccountcontroller.js
@@ -16,33 +16,37 @@
     const email = $('#email').val();
     const userPassword = $('#password').val();
     if (email !== '' && userPassword !== '') {
-      if (userPassword.length < 5) {
-        $('#validateUserAccount').html('Password is weak!!');
+      if (!isEmail(email)) {
+        $('#validateUserAccount').html('Provide a valid email!!');
       } else {
-      // GETTING STARTED
-        $('#sign_up').html('Submitting');
-        $('#sign_up').prop('disabled', true);
-        // AJAX
-        $.ajax({
-          url: '/register-userAccount',
-          method: 'POST',
-          contentType: 'application/json',
-          data: JSON.stringify(
-            {
-              email: email,
-              password: userPassword
-            }),
-          success: function (response) {
-            window.location.href = '/';
-          },
-          error: function (xhr) {
+        if (userPassword.length < 5) {
+          $('#validateUserAccount').html('Password is weak!!');
+        } else {
+          // GETTING STARTED
+          $('#sign_up').html('Submitting');
+          $('#sign_up').prop('disabled', true);
+          // AJAX
+          $.ajax({
+            url: '/register-userAccount',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(
+              {
+                email: email,
+                password: userPassword
+              }),
+            success: function (response) {
+              window.location.href = '/';
+            },
+            error: function (xhr) {
             // eslint-disable-next-line no-eval
-            const err = eval(`(${xhr.responseText})`); // FIXME: Remove use of `eval` as it's considered really bad.
-            $('#validateUserAccount').html(err.message);
-            $('#sign_up').prop('disabled', false);
-            $('#sign_up').html('Sign up');
-          }
-        });
+              const err = eval(`(${xhr.responseText})`); // FIXME: Remove use of `eval` as it's considered really bad.
+              $('#validateUserAccount').html(err.message);
+              $('#sign_up').prop('disabled', false);
+              $('#sign_up').html('Sign up');
+            }
+          });
+        }
       }
     } else {
       $('#validateUserAccount').html('All fields are required !!');
@@ -91,5 +95,10 @@
     } else {
       $('#validateUserAccount').html('All fields are required !!');
     }
+  }
+
+  function isEmail (email) {
+    const regex = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return regex.test(email);
   }
 })(jQuery);
